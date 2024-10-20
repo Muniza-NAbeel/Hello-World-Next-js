7fc48f28f840ed47cb9a861267bba4cd543b4495
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-10-20 10:33:53 +0500

    helo world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-teal-100">
       <h1 className="text-5xl font-bold text-teal-950">
-        "Hello World"
+        Hello World
       </h1>
     </div>
   );
